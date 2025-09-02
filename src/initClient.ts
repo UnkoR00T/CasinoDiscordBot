f50ccab5bb9a1dd22a86d336aa19113ff6712132
UnkoR00T <unkor00t@gmail.com>
2025-09-02 14:55:42 +0200

    Notify channel of bot restart with latest commit message
    Adds a fetch request to retrieve the latest commit message from the GitHub repository and includes it in the restart notification sent to the Discord channel.

--- src/initClient.ts
+++ src/initClient.ts
@@ -21,8 +21,15 @@ const initClient = (token: string) => {
     try {
       let channel = await client.channels.fetch("1411019046597431336");
       let role = "1412417908172259379";
+      let res = await fetch(
+        "https://github.com/UnkoR00T/CasinoDiscordBot/commits/master",
+        {
+          method: "GET",
+        },
+      );
+      let changes = res.json();
       (channel as TextChannel).send({
-        content: `<@&${role}> Bot sie zrestartował, chyba Update. ${spoiler("Restart hajsu do 1000$")}`,
+        content: `<@&${role}> Bot sie zrestartował, chyba Update. ${spoiler("Restart hajsu do 1000$")} \n Changes: \n ${changes.commit.message}`,
       });
     } catch (err) {
       console.error(err);
